bff42ee98915719dba37b373efe56c3d12c4c1ae
Mario Hoyos <mariohoyos92@gmail.com>
2018-01-30 11:08:05 -0600

    chore: finished example for class

--- src/Mentor.js
+++ src/Mentor.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 
 export default class Mentor extends Component {
   render() {
+    console.log(this.props);
     return (
       <p onClick={() => this.props.mentorSelector(this.props.name)}>
         {this.props.name}
